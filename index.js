fcca720c2fd47530a06fe25786429f2212cea864
Gottox <g@s01.de>
2013-05-10 18:18:33 +0200

    export classes directly

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-exports.TermBuffer = require("./lib/term_buffer.js").TermBuffer;
-exports.Terminal = exports.TermBuffer; // legacy 
-exports.TermDiff = require("./lib/term_diff.js").TermDiff;
-exports.TermWriter = require("./lib/term_writer.js").TermWriter;
+module.exports.TermBuffer = require("./lib/term_buffer.js");
+module.exports.Terminal = exports.TermBuffer; // legacy 
+module.exports.TermDiff = require("./lib/term_diff.js").TermDiff;
+module.exports.TermWriter = require("./lib/term_writer.js");
