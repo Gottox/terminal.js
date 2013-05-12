70c4b950ed7b14b704799516a1efca470904eedc
Gottox <g@s01.de>
2013-05-12 20:53:17 +0200

    correctly export TermDiff

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports.TermBuffer = require("./lib/term_buffer.js");
 module.exports.Terminal = exports.TermBuffer; // legacy 
-module.exports.TermDiff = require("./lib/term_diff.js").TermDiff;
+module.exports.TermDiff = require("./lib/term_diff.js");
 module.exports.TermWriter = require("./lib/term_writer.js");
 module.exports.renderer = {
 	PlainRenderer: require("./lib/renderer/plain.js"),
