0b98fc710d7c2baec229c2b8020a59bde3ac1811
Gottox <g@s01.de>
2013-05-14 12:19:13 +0200

    line.attr[x] will only be set, when an attribute is changed

--- test/handler_sgi.js
+++ test/handler_sgi.js
@@ -8,16 +8,18 @@ function newTermWriter(w, h) {
 describe('TermWriter SGI', function() {
 	it("resets attributes", function() {
 		var t = newTermWriter();
-		t.write("\x1b[1mb\x1b[mn");
-		expect(t.buffer._buffer.str[0][0]).to.be('b');
-		expect(t.buffer._buffer.attr[0][0].bold).to.be(true);
-		expect(t.buffer._buffer.str[0][1]).to.be('n');
-		expect(t.buffer._buffer.attr[0][1].bold).to.be(false);
+		t.write("\x1b[1mbb\x1b[mn");
+		var line = t.buffer.getLine(0);
+		expect(line.str).to.be('bbn');
+		expect(line.attr[0].bold).to.be(true);
+		expect(line.attr[1]).to.be(undefined);
+		expect(line.attr[2].bold).to.be(false);
 	});
 	it("sets bold", function() {
 		var t = newTermWriter();
 		t.write("\x1b[1mb");
-		expect(t.buffer._buffer.str[0][0]).to.be('b');
-		expect(t.buffer._buffer.attr[0][0].bold).to.be(true);
+		var line = t.buffer.getLine(0);
+		expect(line.str).to.be('b');
+		expect(line.attr[0].bold).to.be(true);
 	});
 });
