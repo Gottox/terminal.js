31c0b16878c096d34789fbfc5d18ad7073596d53
Patrick Debois <Patrick.Debois@jedi.be>
2013-05-16 19:25:42 +0200

    add vt100 torture data

--- Makefile
+++ Makefile
@@ -86,4 +86,7 @@ mrproper: clean
 	@echo "RM         node_modules"
 	@rm -rf node_modules || true
 
+torture: 
+	@node bin/ansi.js sample-data/vt100test.txt
+
 .PHONY: test test-browser coverage clean mrproper lint
---
+++ bin/ansi.js
@@ -0,0 +1,14 @@
+var TermBuffer = require("../lib/term_buffer.js");
+var TermWriter = require("../lib/term_writer.js");
+var AnsiRenderer = require("../lib/renderer/ansi.js");
+var fs = require('fs')
+var io = require('socket.io-client');
+
+var term_buffer = new TermBuffer(80, 24);
+var term_writer = new TermWriter(term_buffer);
+term_buffer.setMode('crlf',true);
+
+var vt100 = fs.readFileSync(process.argv[2]);
+term_writer.write(vt100);
+var ansi_renderer = new AnsiRenderer(term_buffer);
+console.log(ansi_renderer.toString());
