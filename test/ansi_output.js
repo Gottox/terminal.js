84074216887346eb4c13a146568d354f203e0150
Gottox <g@s01.de>
2013-05-25 18:04:22 +0200

    Show cursor correctly in AnsiOutput.

--- test/ansi_output.js
+++ test/ansi_output.js
@@ -13,8 +13,8 @@ describe('AnsiOutput', function() {
 		t.write("Hello");
 
 		//expect(r.toString()).to.be('\u001b[22;23;24;25;27mHello\u001b[0m');
-		expect(r.toString()).to.be('\u001b[22;23;24;25;27mHello\u001b[7;22;23;24;25;27m \u001b[0m');
+		expect(r.toString()).to.be('\u001b[22;23;24;25;27mHello\u001b[22;23;24;25;7m \u001b[0m');
 		t.buffer.setCursor(0,0);
-		expect(r.toString()).to.be('\u001b[7;22;23;24;25;27mH\u001b[22;23;24;25;27mello\u001b[0m');
+		expect(r.toString()).to.be('\u001b[22;23;24;25;7mH\u001b[22;23;24;25;27mello\u001b[0m');
 	});
 });
