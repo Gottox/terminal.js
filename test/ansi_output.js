a352cf0b6d7914af55a1db189a3a0e6c97771875
Gottox <g@s01.de>
2013-05-25 11:08:17 +0200

    refactor AnsiOutput, now prints cursor.

--- test/ansi_output.js
+++ test/ansi_output.js
@@ -12,6 +12,9 @@ describe('AnsiOutput', function() {
 		var r = new AnsiOutput(t.buffer);
 		t.write("Hello");
 
-		expect(r.toString()).to.be('\u001b[22;23;24;25;27mHello\u001b[0m');
+		//expect(r.toString()).to.be('\u001b[22;23;24;25;27mHello\u001b[0m');
+		expect(r.toString()).to.be('\u001b[22;23;24;25;27mHello\u001b[7;22;23;24;25;27m \u001b[0m');
+		t.buffer.setCursor(0,0);
+		expect(r.toString()).to.be('\u001b[7;22;23;24;25;27mH\u001b[22;23;24;25;27mello\u001b[0m');
 	});
 });
