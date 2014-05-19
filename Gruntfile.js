23050bb306f5cac13eda9829f7752e4c1d61c241
Enno Boland <g@s01.de>
2014-05-19 17:53:28 +0200

    reformat Gruntfile.js, add grunt-jsdoc to package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,13 @@
 var GLOBAL = "Terminal";
-var SRC = [ 'lib/**/*.js' ];
+var SRC = [ "lib/**/*.js" ];
 
 module.exports = function(grunt) {
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON("package.json"),
 		browserify: {
 			release: {
 				files: {
-					'dist/terminal.js': [ "./index.js" ]
+					"dist/terminal.js": [ "./index.js" ]
 				},
 				options: {
 					bundleOptions: {
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
 			},
 			debug: {
 				files: {
-					'dist/terminal.dbg.js': [ "./index.js" ]
+					"dist/terminal.dbg.js": [ "./index.js" ]
 				},
 				options: {
 					bundleOptions: {
@@ -30,15 +30,15 @@ module.exports = function(grunt) {
 		uglify: {
 			all: {
 				files: {
-					'dist/terminal.min.js': ['dist/terminal.js']
+					"dist/terminal.min.js": [ "dist/terminal.js" ]
 				}
 			}
 		},
 		mochaTest: {
 			all: {
-				src: ['test/*.js'],
+				src: [ "test/*.js" ],
 				options: {
-					reporter: 'spec',
+					reporter: "spec",
 					require: ["test/common.js"]
 				}
 			}
@@ -46,20 +46,42 @@ module.exports = function(grunt) {
 		jshint: {
 			all: SRC
 		},
-		clean: ["dist"]
+		jsdoc : {
+			all: {
+				src: SRC,
+				options: {
+					destination: "doc"
+				}
+			}
+		}
+		clean: [ "dist", "doc" ]
 	});
 
-	grunt.loadNpmTasks('grunt-browserify');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-mocha-test');
+	grunt.loadNpmTasks("grunt-browserify");
+	grunt.loadNpmTasks("grunt-contrib-clean");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-jsdoc");
+	grunt.loadNpmTasks("grunt-mocha-test");
 
-	grunt.registerTask('default', ['browserify:release', 'uglify']);
-	grunt.registerTask('test', ['jshint', 'mochaTest' ]);
-	grunt.registerTask('test-browser', ['jshint', 'browserify:debug',
-			'test-browser:inform' ]);
-	grunt.registerTask('test-browser:inform', function() {
-		grunt.log.write('Open file://' + __dirname + "/test/index.html in your browser.").ok();
+	grunt.registerTask("test-browser:inform", function() {
+		grunt.log.write("Open file://" + __dirname +
+				"/test/index.html in your browser.").ok();
 	});
+
+	grunt.registerTask("default", [
+			"browserify:release",
+			"uglify" ]);
+	grunt.registerTask("doc", [
+			"jsdoc"
+	]);
+	grunt.registerTask("test", [
+			"jshint",
+			"mochaTest"
+	]);
+	grunt.registerTask("test-browser", [
+			"jshint",
+			"browserify:debug",
+			"test-browser:inform"
+	]);
 };
