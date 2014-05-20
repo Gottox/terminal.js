7bd3efbbd308729109a09c1f72d5edc2d85700e3
Enno Boland <g@s01.de>
2014-05-20 19:24:13 +0200

    gh-pages.sh: add script to create gh-pages

--- gh-pages.sh
+++ gh-pages.sh
@@ -12,6 +12,7 @@ rev=`git rev-parse HEAD`
 grunt clean || exit $?
 git clone -b gh-pages . doc || exit $?
 grunt doc || exit $?
-git -C doc commit -am "rebuild docs based on $rev" || exit $?
+git -C doc add $PWD/doc
+git -C doc commit -m "rebuild docs based on $rev" || exit $?
 git -C doc push $PWD gh-pages || exit $?
 grunt clean || exit $?
