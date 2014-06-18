574ea6956927dd8d5a3f5fd9aafd0c69e704b5dc
Enno Boland <g@s01.de>
2014-06-18 14:28:32 +0200

    remove examples for now as they are no longer valid.

--- examples/ansi_rendering/app.js
+++
@@ -1,9 +0,0 @@
-var Terminal = require("../../index.js");
-var fs = require('fs');
-
-var terminal = new Terminal(80, 24, 'ansi');
-terminal.buffer.setMode('crlf',true);
-
-var vt100 = fs.readFileSync(process.argv[2]);
-terminal.writer.write(vt100);
-process.stdout.write(terminal.toString());
