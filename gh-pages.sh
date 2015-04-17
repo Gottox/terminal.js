efecee548f61b8b9c97a567958379a48a02a0c8d
Enno Boland <g@s01.de>
2015-04-17 02:18:59 +0200

    gh-pages: add CNAME

--- gh-pages.sh
+++ gh-pages.sh
@@ -12,6 +12,7 @@ rev=`git rev-parse HEAD`
 grunt clean || exit $?
 git clone -b gh-pages . doc || exit $?
 grunt doc || exit $?
+echo terminal.js.org > $PWD/doc/CNAME
 git -C doc add $PWD/doc
 git -C doc commit -m "rebuild docs based on $rev" || exit $?
 git -C doc push $PWD gh-pages || exit $?
